6c1988cc76b3b129292eae68714a6bd68fc0132c
mummj <mummj@eou.edu>
2021-04-15 10:16:19 -0700

    animal farm intial set up

---
+++ script/script.js
@@ -0,0 +1,19 @@
+'use strict'
+
+//console.log("hello world")
+
+//TODO: I want to create a template - constructor function for an animal
+//create a single constructor - template
+//the constructo takes a parameter - animalName
+// property called - animalName
+//method to render itself on the screen - render()
+let createButton = document.getElementById('makeCat');
+
+createButton.addEventListener('click', animalName);
+
+function animalName(){
+    let cat = document.getElementById('makeCat').value
+}
+
+
+animal.render();
