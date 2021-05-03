133a7d83fe415f94c281e9839d128cdd71a66580
mummj <mummj@eou.edu>
2021-05-03 08:08:31 -0700

    working on the flex and gird style for the animal farm

--- script/journal.js
+++ script/journal.js
@@ -1,15 +1,26 @@
 'use script'
 
-//console.log('hello world')
-
-//target the form
 const form = document.getElementsByTagName('FORM')[0];
 
-function makeNewArticle(article){
-    this.article=article;
-}
 
-makeNewArticle.prototype.render =function(){
-    let article = document.createElement('article');
-    article.textContent =  this.form;
+form.addEventListener("submit", makeNewArticle);
+
+
+function makeNewArticle(event){
+    event.preventDefault();
+    const title = event.target.title.value;
+    const journal = event.target.content.value;
+    
+    const article = document.createElement('article');
+    const h2 = document.createElement('h2');
+    const p = document.createElement('p');
+
+    h2.textContent = title;
+    p.textContent = journal;
+
+    article.appendChild(h2);
+    article.appendChild(p);
+    const main = document.querySelector("main");
+    main.appendChild(article);
+    
 }
